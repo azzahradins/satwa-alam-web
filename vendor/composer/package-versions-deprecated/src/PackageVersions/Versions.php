be5a3e4564349609de66a83054da6cd2fd3629e9
Azzahra Dinda S <azzahradindas@gmail.com>
2021-01-13 21:39:41 +0700

    Update

--- vendor/composer/package-versions-deprecated/src/PackageVersions/Versions.php
+++ vendor/composer/package-versions-deprecated/src/PackageVersions/Versions.php
@@ -149,7 +149,7 @@ final class Versions
   'sebastian/type' => '2.3.1@81cd61ab7bbf2de744aba0ea61fae32f721df3d2',
   'sebastian/version' => '3.0.2@c6c1022351a901512170118436c764e473f6de8c',
   'theseer/tokenizer' => '1.2.0@75a63c33a8577608444246075ea0af0d052e452a',
-  'laravel/laravel' => 'dev-main@04a60db4a1ff03ee452746de3dfa8e06ff1bfbb1',
+  'laravel/laravel' => 'dev-main@f9b21b192837217735c99be1281734a380f2ba8d',
 );
 
     private function __construct()
