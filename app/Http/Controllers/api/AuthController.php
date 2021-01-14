a8f0917fad90c403811370b77e7479f52b2ea7db
Azzahra Dinda S <azzahradindas@gmail.com>
2021-01-14 13:12:55 +0700

    update

--- app/Http/Controllers/api/AuthController.php
+++ app/Http/Controllers/api/AuthController.php
@@ -14,7 +14,7 @@ class AuthController extends Controller
 {
     public function __construct()
     {
-        $this->middleware('auth:api', ['except' => ['register', 'login']]);
+        //$this->middleware('auth:api', ['except' => ['register', 'login']]);
     }
     /**
      * Get a JWT via given credentials.
